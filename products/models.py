7098f68648382737e3f16ee71582475969abf739
Alijonovaward <Alijonovaward@gmail.com>
2025-10-25 16:40:25 +0500

    a

--- products/models.py
+++ products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='Nomi')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Kelgan narxi')
     selling_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Sotuv narxi')
-    stock = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Miqdori')
+    stock = models.DecimalField(max_digits=15, decimal_places=2, verbose_name='Miqdori')
     qrcode = models.CharField(max_length=100, unique=True, blank=True, null=True, verbose_name='QR Kod')  # Yangi maydon
 
     def __str__(self):
